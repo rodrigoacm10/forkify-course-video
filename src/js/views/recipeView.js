c8217304d9843e29a5e315c4a881374aab9dfd90
rodrigoacm10 <rodrigoacm10@gmail.com>
2023-06-30 12:35:06 -0300

    finished

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -127,15 +127,12 @@ class RecipeView extends View {
   }
 
   _generateMarkupIngredient(ing) {
-    console.log(ing.quantity);
-    console.log(fracty(ing.quantity));
-
     return `<li class="recipe__ingredient">
    <svg class="recipe__icon">
      <use href="${icons}#icon-check"></use>
    </svg>
    <div class="recipe__quantity">${
-     ing.quantity ? new fracty(ing.quantity).toString() : ''
+     ing.quantity ? fracty(ing.quantity).toString() : ''
    }</div>
    <div class="recipe__description">
      <span class="recipe__unit">${ing.unit}</span>
